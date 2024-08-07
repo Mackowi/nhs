757ed81384a531f78a57b5633ba0f3142c5810ed
Mackowi <dzierzenga.maciej@gmail.com>
2024-08-07 20:59:36 +0200

    carousel finished;

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,12 +2,11 @@ const images = [
   'slider1.png',
   'slider2.png',
   'slider3.png',
+  'slider4.png',
   'slider1.png',
   'slider2.png',
   'slider3.png',
-  'slider1.png',
-  'slider2.png',
-  'slider3.png',
+  'slider4.png',
 ]
 
 const Carousel = () => {
@@ -19,7 +18,7 @@ const Carousel = () => {
             key={index}
             src={src}
             alt={`Image${index + 1}`}
-            className='h-64 w-54 flex-shrink-0'
+            className='h-64 w-54 flex-shrink-0 mx-8'
             // className='w-full h-64 w-40 flex-shrink-0'
           />
         ))}
